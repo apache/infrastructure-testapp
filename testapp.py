f48e85fd0cdbbcdba97baf98d3e4794e6bae1bfd
Daniel Gruno <humbedooh@apache.org>
2020-06-14 19:59:58 +0200

    Create testapp.py

---
+++ testapp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Super simple test application for pipservice installer """
+import certifi    # Testing pipenv installed the mods
+import netaddr    # Testing more...
+import time
+
+print("This works!")
+while True:
+    time.sleep(60)
+    print("test app still works!")
